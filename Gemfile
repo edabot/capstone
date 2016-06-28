ad71ef604a866d7a255f5353dc5d110bd7e2a87e
App Academy Student <student@appacademy.io>
2016-06-28 11:09:39 -0700

    react frontend skeleton in place

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+gem 'gon'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
@@ -38,8 +39,10 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-rails'
+  gem 'annotate'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
---
+++ frontend/foodium.jsx
@@ -0,0 +1,37 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactRouter = require('react-router');
+const Route = ReactRouter.Route;
+const Router = ReactRouter.Router;
+const IndexRoute = ReactRouter.IndexRoute;
+const hashHistory = ReactRouter.hashHistory;
+
+const App = React.createClass({
+  getChildContext() {
+    return {router: hashHistory};
+  },
+
+  render() {
+    return(
+      <div>
+        <header><h1>Foodium</h1></header>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+App.childContextTypes = {
+  router: React.PropTypes.object
+};
+
+const routes=(
+  <Route path='/' component={App}>
+  </Route>
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <Router history={hashHistory}>{routes}</Router>,
+    document.getElementById("content"));
+});
