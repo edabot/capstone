1d8541e7c5331c9a1d1ad2d5674b6fafe2529faa
edabot <ed.lewis@gmail.com>
2016-06-29 11:12:14 -0700

    login/signup and username in navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ const Modal = require('react-modal');
 const LoginForm = require('./login_form');
 const FormStyle = require('../styles/form_style');
 const FormActions = require('../actions/form_actions');
+
 const App = React.createClass({
   getInitialState(){
     return {
@@ -42,6 +43,25 @@ const App = React.createClass({
     }
   },
 
+  navUserSection(){
+    let user = this.state.currentUser.username;
+    if (user !== undefined) {
+      return(
+        <div>
+        <li>{user}</li>
+        <li><button onClick={SessionActions.logout}>logout</button></li>
+        </div>
+      );
+    } else {
+      return(
+        <div>
+        <li><button type="button" onClick={this._handleLogInClick}>Log In</button></li>
+        <li><button type="button" onClick={this._handleSignUpClick}>Sign Up</button></li>
+        </div>
+      );
+    }
+  },
+
   attractBox(){
     if(this.state.currentUser.username === undefined) {
       return(
@@ -67,15 +87,32 @@ const App = React.createClass({
     return(
       <div>
 
-        <form className="navbar-form navbar-left" role="search">
-          <div className="form-group">
-            <input type="text" className="form-control" placeholder="Search" />
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Foodium</a>
+            </div>
+
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <form className="navbar-form navbar-left" role="search">
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Search" />
+                </div>
+                <button type="submit" className="btn btn-default">Submit</button>
+              </form>
+              <ul className="nav navbar-nav navbar-right">
+                {this.navUserSection()}
+              </ul>
+            </div>
           </div>
-          <button type="submit" className="btn btn-default">Submit</button>
-        </form>
-        <header><h1>Foodium</h1>
-        {this.userDisplay()}
-        </header>
+        </nav>
+
         <Modal
           isOpen={this.state.modalOpen}
           onRequestClose={this._handleClose}
