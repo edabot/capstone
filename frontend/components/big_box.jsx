90f23308870b4dfa5131411e6d4cad2fed6cfbce
edabot <ed.lewis@gmail.com>
2016-06-29 11:45:02 -0700

    promo box added

---
+++ frontend/components/big_box.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+const Author = React.createClass({
+  render(){
+    return(
+      <div class="jumbotron">
+        <h1>Hello, world!</h1>
+        <p>...</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+      </div>
+    );
+  }
+});
+
+module.exports = Author;
