1a53c816b2ff3fb8fac0b7fdcac1dcb77810cb10
edabot <ed.lewis@gmail.com>
2016-06-30 11:33:19 -0700

    added recipe index

--- frontend/components/recipe_index.jsx
+++ frontend/components/recipe_index.jsx
@@ -1,11 +1,36 @@
 const React = require('react');
+const RecipeStore = require('../stores/recipe_store');
+const RecipeActions = require('../actions/recipe_actions');
+const RecipeIndexItem = require('./recipe_index_item');
 
-const Index = React.createClass({
+const RecipeIndex = React.createClass({
+  getInitialState(){
+    return {recipes: [] };
+  },
+  componentDidMount(){
+    this.storeListener = RecipeStore.addListener(this._updateRecipes);
+    RecipeActions.getRecipeIndex();
+  },
+  _updateRecipes(){
+    console.log("updateRecipes");
+    this.setState({recipes: RecipeStore.getRecipes()});
+  },
+  componentWillUnmount(){
+    this.storeListener.remove();
+  },
   render(){
     return(
-      <div>Index</div>
+      <div className="recipe-index">
+        {
+          this.state.recipes.map( recipe => {
+            return (
+              <RecipeIndexItem recipe={recipe} />
+            );
+          })
+        }
+      </div>
     );
   }
 });
 
-module.exports = Index;
+module.exports = RecipeIndex;
