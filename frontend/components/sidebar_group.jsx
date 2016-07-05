3d660aa521a28aab9140acc1fccb4333a751bb55
edabot <ed.lewis@gmail.com>
2016-07-05 13:48:18 -0700

    sidebar category names link to tag indexes

--- frontend/components/sidebar_group.jsx
+++ frontend/components/sidebar_group.jsx
@@ -1,12 +1,13 @@
 const React = require("react");
 const SidebarItem = require('./sidebar_item');
+const Link = require('react-router').Link
 
 const SidebarGroup = React.createClass({
   render(){
     return (
       <div>
         <div className="sidebar-category">
-          {this.props.group.name}
+          <Link to={"tags/" + this.props.group.name}>{this.props.group.name}</Link>
         </div>
         <div className="sidebar-recipes">
           {this.props.group.recipes.map(recipe => {
